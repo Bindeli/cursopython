b5258d774c38a0bad345967938b9ff0510cde9fd
Bindeli <lucasbin2204@gmail.com>
2022-05-25 23:14:57 -0300

    25052022p

--- intermediario/validarcnpj.py
+++ intermediario/validarcnpj.py
@@ -1,14 +1,23 @@
 """
-04.252.011/0001-10 40.688.134/0001-61 71.506.168/0001-11 12.544.992/0001-05
+04.252.011/0001-10 (esse tem uma regrinha) (A validação é feito por esses números iniciais 04.252.011/0001 )
+antes do ' - '
+40.688.134/0001-61
+71.506.168/0001-11
+12.544.992/0001-05
+
+pode escolher qualquer um dos 4 cnpj's
+tirar os pontos e as barras
+e abrir desse jeito :
 
 0   4   2   5   2   0   1   1   0   0   0   1
-5   4   3   2   9   8   7   6   5   4   3   2
-0   16  6   10  18  0   7   6   0   0   0   2 = 65
+5   4   3   2   9   8   7   6   5   4   3   2 (essa é uma sequencia que vai de 5 a 2 e 9 a 6 e 5 a 2 dnv)
+x ---------------------------------------------
+0   16  6   10  18  0   7   6   0   0   0   2 = 65 (vai somar o resultado da multiplicação e somar )
 Fórmula -> 11 - (65 % 11) = 1
 Primeiro digito = 1 (Se o digito for maior que 9, ele se torna 0)
 
 0   4   2   5   2   0   1   1   0   0   0   1   1   X
-6   5   4   3   2   9   8   7   6   5   4   3   2
+6   5   4   3   2   9   8   7   6   5   4   3   2 (uma outra sequencia)
 0   20  8   15  4   0   8   7   0   0   0   3   2 = 67
 Fórmula -> 11 - (67 % 11) = 10 (Como o resultado é maior que 9, então é 0)
 Segundo digito = 0
